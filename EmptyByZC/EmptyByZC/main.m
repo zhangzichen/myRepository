0d54c7a9d2c444852b56bcd3a3ca73ae96e0f8cd
张飞 <1015947235@qq.com>
2015-11-09 09:52:01 +0800

    11-09 提交一个空工程

---
+++ EmptyByZC/EmptyByZC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EmptyByZC
+//
+//  Created by lanou3g on 15/11/9.
+//  Copyright © 2015年 ZiChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
