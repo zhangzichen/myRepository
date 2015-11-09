175bb774dc0eef87081ff221e9f359a3885fb532
张飞 <1015947235@qq.com>
2015-11-09 10:13:53 +0800

    asdas

--- EmptyByZC/EmptyByZC/AppDelegate.m
+++ EmptyByZC/EmptyByZC/AppDelegate.m
@@ -16,6 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    NSLog(@"sssssssssssssss");
     // Override point for customization after application launch.
     return YES;
 }
